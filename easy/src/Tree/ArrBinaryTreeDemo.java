8dc9c3ff632113053025dbadfd362f181cd5ab67
xiehaizheng <1076741748@qq.com>
2020-05-18 11:15:47 +0800

    437

--- easy/src/Tree/ArrBinaryTreeDemo.java
+++ easy/src/Tree/ArrBinaryTreeDemo.java
@@ -7,8 +7,10 @@ public class ArrBinaryTreeDemo {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        //创建一个 ArrBinaryTree
-//        ArrBinaryTree arrBinaryTree = new ArrBinaryTree(arr);
+//        创建一个 ArrBinaryTree
+        ArrBinaryTree arrBinaryTree = new ArrBinaryTree(arr);
+        arrBinaryTree.preOrder(0);
+
 
     }
 
